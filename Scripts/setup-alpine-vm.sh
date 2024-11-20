07de5200671c505f967f0882569d7d15f70ee516
Alessio Giordano <46226677+alessiogiordano@users.noreply.github.com>
2024-11-20 23:04:35 +0100

    Create setup-alpine-vm.sh
    alpine-standard-3.20.3-aarch64

---
+++ Scripts/setup-alpine-vm.sh
@@ -0,0 +1,23 @@
+vi /etc/apk/repositories # Decomment community repo
+apk update
+apk upgrade
+apk add docker
+rc-update add docker boot
+apk add open-vm-tools open-vm-tools-guestinfo open-vm-tools-deploypkg open-vm-tools-plugins-all
+rc-update add open-vm-tools boot
+mkdir shared
+modprobe fuse
+/usr/bin/vmhgfs-fuse .host:/shared /root/shared
+apk add python3 py3-pip
+apk add curl
+vi /etc/inittab # tty1::respawn:/bin/sh
+apk add vim
+echo "#!/sbin/openrc-run" >> /etc/init.d/mount_shared
+echo "start() {" >> /etc/init.d/mount_shared
+echo "ebegin \"Mounting shared directory\"" >> /etc/init.d/mount_shared
+echo "modprobe fuse" >> /etc/init.d/mount_shared
+echo "/usr/bin/vmhgfs-fuse .host:/shared /root/shared" >> /etc/init.d/mount_shared
+echo "eend $?" >> /etc/init.d/mount_shared
+echo "}" >> /etc/init.d/mount_shared
+chmod +x /etc/init.d/mount_shared
+rc-update add mount_shared boot
